0c594bcd1ed24fd76b073f02230bafdcda7e53d4
Art395 <artem.chuev@tktk.ee>
2024-12-19 17:50:59 +0200

    first commit

---
+++ Grade/Program.cs
@@ -0,0 +1,29 @@
+﻿char userGrade;
+Console.WriteLine("Enter your grade:");
+userGrade = Convert.ToChar(Console.ReadLine());
+
+switch (userGrade)
+{
+    case '0':
+        Console.WriteLine("Incomplete.");
+        break;
+    case '1':
+        Console.WriteLine("Meager.");
+        break;
+    case '2':
+        Console.WriteLine("Satisfying.");
+        break;
+    case '3':
+        Console.WriteLine("Good!");
+        break;
+    case '4':
+        Console.WriteLine("Very good!");
+        break;
+    case '5':
+        Console.WriteLine("Excellent!");
+        break;
+        default:
+        Console.WriteLine("Wrong value!");
+        break;
+}
+
